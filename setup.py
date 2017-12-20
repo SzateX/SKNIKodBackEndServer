5bedd899b6c37cc846862918a18d07885b01f3c4
SzateX <mistrzswiata.salata@gmail.com>
2017-12-20 23:14:56 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="KoloRESTApi",
+    version="0.0.1",
+    author="SKNI KOD",
+    author_email="jaksza18@gmail.com",
+    description=("REST API for SKNI KOD Website"),
+    install_requires=['django', 'djangorestframework', 'markdown', 'django-filter'],
+    license="MiT",
+    keywords="www website etc",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MiT",
+    ]
+)
