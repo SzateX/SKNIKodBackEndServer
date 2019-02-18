0ea392dad5bc6a114f8d46328f4029d15d4c2178
Paweł Dziedzic <blavikenn@gmail.com>
2019-02-18 13:35:20 +0100

    Add first and last name to User serializer
    Neede for registration

--- RESTApi/serializers.py
+++ RESTApi/serializers.py
@@ -7,7 +7,7 @@ from .models import Profile, RepoLink, Article, Comment, Tag, ArticleAuthor, \
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'groups', 'profile', 'password')
+        fields = ('id', 'username', 'email', 'groups', 'profile', 'password', 'first_name', 'last_name')
         read_only_fields = ('profile', 'groups')
         extra_kwargs = {
             'password': {'write_only': True}
@@ -16,7 +16,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
