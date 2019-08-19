a012aea4df589da9821e16caf5f557f647a302e9
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-08-19 21:10:18 +0200

    Corrected settings.py

--- KoloRESTApi/settings.py
+++ KoloRESTApi/settings.py
@@ -166,6 +166,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafolder")
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfolder")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfolder")
 ]
