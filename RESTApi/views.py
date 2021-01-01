0ef763b9456644f935ae17d2da0621476cd4a785
Kazan1520 <kazunnoto@gmail.com>
2021-01-01 23:25:36 +0100

    Revert "Update views.py"
    This reverts commit e577ef6e16695ff9d054e93a6b9a5f7ed6151473.

--- RESTApi/views.py
+++ RESTApi/views.py
@@ -2,10 +2,8 @@ import django_filters
 from django.contrib.auth.models import User, Group
 from django.views.generic import TemplateView
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 from .models import *
 from RESTApi.serializers import *
@@ -53,49 +51,6 @@ class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-
-class GroupViewSetDetail(APIView):
-    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
-
-    def get(self, request, pk=None, format=None):
-        queryset = Group.objects.get(pk=pk)
-        serializer = GroupSerializer(queryset)
-        return Response(serializer.data)
-
-    def put(self, request, pk=None, format=None):
-        queryset = Group.objects.get(pk=pk)
-        serializer = GroupSerializer(queryset)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk=None, format=None):
-        queryset = Group.objects.get(pk=pk)
-        queryset.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def patch(self, request,  pk=None, format=None):
-        queryset = Group.objects.get(pk=pk)
-        serializer = GroupSerializer(queryset, data=request.data, partial=True)
-        if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class GroupViewSetList(APIView):
-    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
-
-    def get(self):
-        queryset = Group.objects.all()
-        serializer = GroupSerializer(queryset)
-        return Response(serializer.data)
-
-    def post(self):
-        serializer = GroupSerializer()
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
