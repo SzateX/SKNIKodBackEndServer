b64c08b3285e3428eaaf88cd170df5f0f1d127ae
Konrad <k.szpytma3@gmail.com>
2021-03-08 12:23:12 +0100

    Fix custom permission

--- RESTApi/custom_permissions.py
+++ RESTApi/custom_permissions.py
@@ -16,7 +16,7 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS or
+            request.method in permissions.SAFE_METHODS or
             request.user and
             request.user.is_staff
         )
