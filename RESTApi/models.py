31d51ca87ce5a337e5c9d5466b549bd6d7074b4c
Kazan1520 <kazunnoto@gmail.com>
2021-05-05 12:38:15 +0200

    Update models.py

--- RESTApi/models.py
+++ RESTApi/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     description = models.TextField(null=True, blank=True)
     avatar = models.ImageField(upload_to='avatars/', null=True)
-    index_number = models.CharField(max_length=6, default=None)
+    index_number = models.CharField(max_length=6, default=None, null=True)
 
     def __str__(self):
         return self.user.username
